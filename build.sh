9b107fa5c0e3d6668619f8e0eaca553118f3b921
Sandeep <sandeep@opstree.com>
2022-11-21 20:11:00 +0530

    Fixed the location of workspace

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
 
 WORKSPACE_PUBLISH_DIR="${EXECUTION_DIR}/${EXECUTION_TASK_ID}"
-logInfoMessage "I'll publish files available at ${FOLDER_TO_BE_COPIED} to [$WORKSPACE_PUBLISH_DIR]"
-cp -r "${FOLDER_TO_BE_COPIED}" "${WORKSPACE}"/"${WORKSPACE_PUBLISH_DIR}"
+SOURCE_FOLDER="${WORKSPACE}"/"${FOLDER_TO_BE_COPIED}"
+logInfoMessage "I'll publish files available at ${FOLDER_TO_BE_COPIED} to [$SOURCE_FOLDER]"
+cp -r "${SOURCE_FOLDER}" "${WORKSPACE_PUBLISH_DIR}"
 sleep  "$SLEEP_DURATION"
