dcf4456ad5231ca93782286f7bd69fefb3b699bd
Sandeep <sandeep@opstree.com>
2022-11-21 20:19:00 +0530

    Added env variable statement

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ source /opt/buildpiper/shell-functions/log-functions.sh
 
 WORKSPACE_PUBLISH_DIR="${EXECUTION_DIR}/${EXECUTION_TASK_ID}"
 SOURCE_FOLDER="${WORKSPACE}"/"${FOLDER_TO_BE_COPIED}"
-logInfoMessage "I'll publish files available at ${FOLDER_TO_BE_COPIED} to [$SOURCE_FOLDER]"
+logInfoMessage "I'll publish files available at ${SOURCE_FOLDER} to [$WORKSPACE_PUBLISH_DIR]"
 cp -r "${SOURCE_FOLDER}" "${WORKSPACE_PUBLISH_DIR}"
 sleep  "$SLEEP_DURATION"
