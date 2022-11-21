4c0c3c37325cabd7342edc40d3bad538d1cf385b
Sandeep <sandeep@opstree.com>
2022-11-21 20:05:56 +0530

    Fixed build script

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ source /opt/buildpiper/shell-functions/log-functions.sh
 
 WORKSPACE_PUBLISH_DIR="${EXECUTION_DIR}/${EXECUTION_TASK_ID}"
 logInfoMessage "I'll publish files available at ${FOLDER_TO_BE_COPIED} to [$WORKSPACE_PUBLISH_DIR]"
-cp -r "${FOLDER_TO_BE_COPIED}" "${WORKSPACE_PUBLISH_DIR}"
+cp -r "${FOLDER_TO_BE_COPIED}/*" "${WORKSPACE}"/"${WORKSPACE_PUBLISH_DIR}/"
 sleep  "$SLEEP_DURATION"
