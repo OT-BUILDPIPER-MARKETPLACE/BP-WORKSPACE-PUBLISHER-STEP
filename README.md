0b53b880da73f35356e5c12ee08ad8d16850a8fc
Sandeep <sandeep@opstree.com>
2022-12-22 04:32:19 +0530

    Updated code to refer the base location

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BP-WORKSPACE-PUBLISHER-STEP
 I'll publish files to workspace so that they can be visible
+
+
+## Setup
+* Clone the code available at [BP-WORKSPACE-PUBLISHER-STEP](https://github.com/OT-BUILDPIPER-MARKETPLACE/BP-WORKSPACE-PUBLISHER-STEP)
+* Build the docker image
+
+```
+git submodule init
+git submodule update
+docker build -t ot/workspace-publisher:0.1 .
+```
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
 
 WORKSPACE_PUBLISH_DIR="${EXECUTION_DIR}/${EXECUTION_TASK_ID}"
-SOURCE_FOLDER="${WORKSPACE}"/"${FOLDER_TO_BE_COPIED}"
+SOURCE_FOLDER="${WORKSPACE}"/"${CODEBASE_DIR}"/"${FOLDER_TO_BE_COPIED}"
 logInfoMessage "I'll publish files available at ${SOURCE_FOLDER} to [$WORKSPACE_PUBLISH_DIR]"
 cp -r "${SOURCE_FOLDER}" "${WORKSPACE_PUBLISH_DIR}"
 sleep  "$SLEEP_DURATION"
